e3ee0db8fd851dc7930ed3b1e8836187e61dc076
sophi2107 <sophia.rivera25@compscihigh.org>
2025-04-04 14:31:00 +0000

    done

--- public/script.js
+++ public/script.js
@@ -2,3 +2,23 @@
 // Make sure that the function takes the name of the potion as a parameter
 // There are already potion's with the label polyjuice or amortentia
 
+async function patchCreature(color) {
+    try {
+        // fill in the path
+        const response = await fetch(base + "/update/" + color, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+               color:"pearlywhite",
+            })
+
+        });
+        const json = await response.json();
+        console.log(json);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+patchPotion("polyjuice")
